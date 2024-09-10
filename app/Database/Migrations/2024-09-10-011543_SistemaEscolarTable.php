60ed326ff02f794fa5b349cd1cf9fcaa23d75fc0
niiiela <danielavieira2024@gmail.com>
2024-09-09 22:33:43 -0300

    .

--- app/Database/Migrations/2024-09-10-011543_SistemaEscolarTable.php
+++ app/Database/Migrations/2024-09-10-011543_SistemaEscolarTable.php
@@ -8,7 +8,45 @@ class SistemaEscolarTable extends Migration
 {
     public function up()
     {
-        //
+        // Create Sistema Escolar table
+
+$this->forge->addField([
+	'id' => [
+	'type' => 'INT',
+	'constraint' => 11,
+	'auto_increment' => true
+	],
+	'name' => [
+	'type' => 'VARCHAR',
+	'constraint' => 50,
+	],
+	'address' => [
+	'type' => 'VARCHAR',
+	'constraint' => 250,
+	],
+	'phone' => [
+	'type' => 'VARCHAR',
+	'constraint' => 20,
+	],
+	'email' => [
+	'type' => 'VARCHAR',
+	'constraint' => 50,
+	],
+	'created_at' => [
+		'type' => 'DATETIME',
+		'null' => true
+	],
+	'update_at' => [
+		'type' => 'DATETIME',
+		'null' => true
+	],
+	'deleted_at' => [
+		'type' => 'DATETIME',
+		'null' => true
+	]
+]);
+$this->forge->addKey('id', true);
+$this->forge->createTable('sistemaescolar');
     }
 
     public function down()
